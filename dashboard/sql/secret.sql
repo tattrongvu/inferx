221bfa80c7f29191b26c52df35e5f1ce62171fe9
inferx <brad.sun@inferx.net>
2025-04-15 09:56:17 -0700

    add group

--- dashboard/sql/secret.sql
+++ dashboard/sql/secret.sql
@@ -1,12 +1,18 @@
 --DROP TABLE ApiKey;
 CREATE TABLE Apikey (
     apikey          VARCHAR NOT NULL,
-    realm           VARCHAR NOT NULL,
     username        VARCHAR NOT NULL,
     keyname         VARCHAR NOT NULL,
     createtime      TIMESTAMP,
     PRIMARY KEY(apikey)
 );
 
-CREATE UNIQUE INDEX apikey_idx_realm_username ON Apikey (realm, username, keyname);
+CREATE UNIQUE INDEX apikey_idx_realm_username ON Apikey (username, keyname);
 
+CREATE TABLE UserGroup (
+    username        VARCHAR NOT NULL,
+    groupname       VARCHAR NOT NULL,
+    PRIMARY KEY(username, groupname)
+);
+
+CREATE INDEX usergroup_idx_groupname ON UserGroup (groupname);
